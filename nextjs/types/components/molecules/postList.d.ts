b4fba82e95fdb04fbbfaa7f94fdf45143e3d64dd
Keita Sato <info.kt310@gmail.com>
2022-03-23 21:31:18 +0900

    type定義

---
+++ nextjs/types/components/molecules/postList.d.ts
@@ -0,0 +1,14 @@
+export type ListsTypes = {
+  posts: array
+}
+
+export type ListTypes = {
+  link: string
+  modified: string
+  title: {
+    rendered: string
+  }
+  excerpt: {
+    rendered: string
+  }
+}
