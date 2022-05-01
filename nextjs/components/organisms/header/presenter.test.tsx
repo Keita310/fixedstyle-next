fc3f4caab7389612405699d17542a7487b78fdf2
Keita Sato <info.kt310@gmail.com>
2022-05-01 13:31:53 +0900

    不要ファイル削除

--- nextjs/components/organisms/header/presenter.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as renderer from 'react-test-renderer'
-import Presenter from './presenter'
-
-it('スナップショットテスト', () => {
-  const description = 'ピストバイク初心者 パーツやトリック、カスタムのことなら【Fixed Style】'
-  const tree = renderer.create(<Presenter description={description} />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
--- nextjs/pages/indexのコピー.tsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <div>ようこそ Next.js へ！</div>
-}
-
-export default Home
