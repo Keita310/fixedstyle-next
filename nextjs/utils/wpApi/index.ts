5628c1d12609dd607cf9f36cecd20732be519916
keita sato home <info.kt310@gmail.com>
2022-09-19 14:16:33 +0900

    tweak

--- nextjs/utils/wpApi/index.ts
+++ nextjs/utils/wpApi/index.ts
@@ -33,10 +33,7 @@ export async function getPosts() {
  */
 export async function getAllPosts() {
   const res = await fetch(`${process.env.WP_REST_API}/wp-json/wp/v2/posts/`)
-  const json = await res.json()
-  return {
-    posts: json,
-  }
+  return await res.json()
 }
 
 // こんな感じで同じように定義しておけばimport時読み込める
