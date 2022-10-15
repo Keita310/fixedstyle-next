1e1913eb57623d3c5644ffb3ad6a68ad6f85a0da
keita sato home <info.kt310@gmail.com>
2022-10-15 18:47:50 +0900

    imgのコンポネント化

---
+++ nextjs/components/img.tsx
@@ -0,0 +1,47 @@
+const Img = ({post}: {post: any}) => {
+  if (!post.post_eyecatch) {
+    return (<></>)
+  }
+  return (
+/*
+            <img
+              src={post.post_eyecatch.st_thumb150[0]}
+              className='attachment-thumbnail size-thumbnail wp-post-image'
+              alt={post.post_title}
+              srcSet={
+                post.post_eyecatch.st_thumb150[0] +
+                ' ' +
+                post.post_eyecatch.st_thumb150[1] +
+                'w, ' +
+                post.post_eyecatch.st_thumb100[0] +
+                ' ' +
+                post.post_eyecatch.st_thumb100[1] +
+                'w'
+              }
+              sizes={
+                '(max-width: ' +
+                post.post_eyecatch.st_thumb150[1] +
+                'px) 100vw, ' +
+                post.post_eyecatch.st_thumb150[1] +
+                'px'
+              }
+              width={post.post_eyecatch.st_thumb150[1]}
+              height={post.post_eyecatch.st_thumb150[1]}
+            />
+*/
+
+
+    <img
+    width={post.post_eyecatch.st_thumb150[1]}
+    height={post.post_eyecatch.st_thumb150[2]} 
+    src={post.post_eyecatch.st_thumb150[0]}
+    className="attachment-thumbnail size-thumbnail wp-post-image"
+    alt={post.post_title}
+    srcSet={`${post.post_eyecatch.st_thumb150[0]} ${post.post_eyecatch.st_thumb150[1]}w,
+    ${post.post_eyecatch.st_thumb100[0]} ${post.post_eyecatch.st_thumb100[1]}w`}
+    sizes={`(max-width: ${post.post_eyecatch.st_thumb150[1]}px) 100vw, ${post.post_eyecatch.st_thumb150[1]}px`}
+  />
+  )
+}
+
+export default Img
--- nextjs/components/molecules/postList/presenter.tsx
+++ nextjs/components/molecules/postList/presenter.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import {URLS} from 'const'
+import Img from 'components/img'
 import Categories from 'components/categories'
 import Tags from 'components/atoms/tags'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -13,30 +14,7 @@ const list = (post: ListTypes, key: number) => {
       <dt>
         <Link href={URLS.SITE + post.post_permalink}>
           <a>
-            <img
-              src={post.post_eyecatch.st_thumb150[0]}
-              className='attachment-thumbnail size-thumbnail wp-post-image'
-              alt={post.post_title}
-              srcSet={
-                post.post_eyecatch.st_thumb150[0] +
-                ' ' +
-                post.post_eyecatch.st_thumb150[1] +
-                'w, ' +
-                post.post_eyecatch.st_thumb100[0] +
-                ' ' +
-                post.post_eyecatch.st_thumb100[1] +
-                'w'
-              }
-              sizes={
-                '(max-width: ' +
-                post.post_eyecatch.st_thumb150[1] +
-                'px) 100vw, ' +
-                post.post_eyecatch.st_thumb150[1] +
-                'px'
-              }
-              width={post.post_eyecatch.st_thumb150[1]}
-              height={post.post_eyecatch.st_thumb150[1]}
-            />
+            <Img post={post} />
           </a>
         </Link>
       </dt>
