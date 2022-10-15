ce2adf40f91c45969661c58b7a9b6ae1126a2c22
keita sato home <info.kt310@gmail.com>
2022-10-15 18:36:08 +0900

    関連リストを動的に

--- nextjs/components/kanren.tsx
+++ nextjs/components/kanren.tsx
@@ -1,23 +1,41 @@
+import Link from 'next/link'
+import {URLS} from 'const'
+
 const Kanren = ({posts}: {posts: any}) => {
   return (
     <>
       <p className="point"><span className="point-in">関連記事</span></p>
       <div className="kanren">
-        <dl>
-          <dt>
-            <a href="https://fixedstyle.net/customize/throttle_brake/">
-              <img width="150" height="150" src="https://fixedstyle.net/wp-content/uploads/2020/01/13-150x150.jpg" className="attachment-thumbnail size-thumbnail wp-post-image" alt="" srcSet="https://fixedstyle.net/wp-content/uploads/2020/01/13-150x150.jpg 150w, https://fixedstyle.net/wp-content/uploads/2020/01/13-100x100.jpg 100w" sizes="(max-width: 150px) 100vw, 150px" />
-            </a>
-          </dt>
-          <dd>
-            <p className="kanren-t">
-              <a href="https://fixedstyle.net/customize/throttle_brake/">スロットルブレーキカスタム</a>
-            </p>
-            <div className="smanone2">
-              <p>オートバイのアクセルのことを主に指します。 グリップをヒネることでワイヤーが引っ張られ、その動作でエンジンの回転数があがります。 (正確には燃料調整機能のことですがここではグリップ操作機構のこととしま &#8230; </p>
-            </div>
-          </dd>
-        </dl>
+        {posts.map((post, index) => (
+          <dl key={index}>
+            <dt>
+              <Link href={URLS.SITE + post.post_permalink}>
+                <a>
+                  <img
+                    width={post.post_eyecatch.st_thumb150[1]}
+                    height={post.post_eyecatch.st_thumb150[2]} 
+                    src={post.post_eyecatch.st_thumb150[0]}
+                    className="attachment-thumbnail size-thumbnail wp-post-image"
+                    alt={post.post_title}
+                    srcSet={`${post.post_eyecatch.st_thumb150[0]} ${post.post_eyecatch.st_thumb150[1]}w,
+                    ${post.post_eyecatch.st_thumb100[0]} ${post.post_eyecatch.st_thumb100[1]}w`}
+                    sizes={`(max-width: ${post.post_eyecatch.st_thumb150[1]}px) 100vw, ${post.post_eyecatch.st_thumb150[1]}px`}
+                  />
+                </a>
+              </Link>
+            </dt>
+            <dd>
+              <p className="kanren-t">
+                <Link href={URLS.SITE + post.post_permalink}>
+                  <a>{post.post_title}</a>
+                </Link>
+              </p>
+              <div className="smanone2">
+                <p>{post.post_excerpt}</p>
+              </div>
+            </dd>
+          </dl>
+        ))}
       </div>
     </>
   )
